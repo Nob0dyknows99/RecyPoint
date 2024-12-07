4a01415574b97ec136a50e4bfba8a5eb90ef13e9
Luis Arevalo <luis.arevalo@alu.ucm.cl>
2024-12-07 20:25:57 -0300

    Update index.js

--- http/index.js
+++ http/index.js
@@ -1,25 +1,31 @@
 import axios from 'axios';
 
-const URL = 'https://recypoint-8c059-default-rtdb.firebaseio.com';
+const URL = 'https://puntoslimpios-42c75-default-rtdb.firebaseio.com';
 
 export async function getPuntosLimpios() {
-    const response = await axios.get(`${URL}/Puntos Limpios.json`);
-    console.log('Respuesta completa de Firebase:', response.data);
+    try {
+        const response = await axios.get(`${URL}/Puntos Limpios.json`);
+        console.log('Respuesta completa de Firebase:', response.data);
 
-    const puntosLimpios = [];
-    for (const key in response.data) {
-        const punto = response.data[key];
-        if (punto) {
-            const obj = {
-                id: key,
-                nombre: punto.Nombre,
-                Latitud: punto.Latitud,
-                Longitud: punto.Longitud,
-            };
-            puntosLimpios.push(obj);
+        const puntosLimpios = [];
+        for (const key in response.data) {
+            const punto = response.data[key];
+            if (punto && punto.Estado && punto.Nombre && punto.Latitud && punto.Longitud) {
+                puntosLimpios.push({
+                    id: key,
+                    Estado: punto.Estado,
+                    nombre: punto.Nombre,
+                    Latitud: punto.Latitud,
+                    Longitud: punto.Longitud,
+                });
+            }
         }
-    }
 
-    console.log('Puntos Limpios procesados:', puntosLimpios); 
-    return puntosLimpios;
+        console.log('Puntos Limpios procesados:', puntosLimpios); 
+        return puntosLimpios;
+
+    } catch (error) {
+        console.error('Error al obtener los Puntos Limpios:', error);
+        return [];
+    }
 }
